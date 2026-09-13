ff36350e17a8cdc11c14236ece1d049d4155fea0
sadegh shokhan <sadegh_shokhan@yahoo.com>
2026-09-13 17:27:17 +0330

    win logic wrote

--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,83 @@ import Log from "./Component/Log.jsx"
 import { WINNING_COMBINATIONS } from "./winning-combinations.js"
 
 
+//________________gameBoard__________
+const initialGameboard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+//|______________________________________________|
+
+//************************************************
+
 //________________compute activePlayer__________
-function deriveActivePlayer(gameTurns){
-    let currentPlayer = "X"
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X"
 
-    if(gameTurns.length > 0 && gameTurns[0].player === "X"){
-      currentPlayer = "O"
-    }
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O"
+  }
 
-return currentPlayer
-}      
+  return currentPlayer
+}
 //|______________________________________________|
 
 
+
 function App() {
-// const [activePlayer , setActivePlayer] = useState("X");
-const[gameTurns , setGameTurns] = useState([])
+  // const [activePlayer , setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([])
 
-const activePlayer = deriveActivePlayer(gameTurns)
+  const activePlayer = deriveActivePlayer(gameTurns)
 
-function handleChangeSymbol(rowIndex , colIndex){
-  // setActivePlayer(prevState => prevState==="X" ? "O" : "X")
-  setGameTurns(prevTurn=> {
-    const currentPlayer = deriveActivePlayer(prevTurn)
+  let gameboard = initialGameboard
 
-    const updateTurn = [{square: {row :rowIndex , col: colIndex} , player:currentPlayer}, ...prevTurn]
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
 
-    return updateTurn
-  })
-}
+    gameboard[row][col] = player
+  }
+
+  let winner = null;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameboard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameboard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameboard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+      ){
+        winner = firstSquareSymbol;
+      }
+    }
+
+
+  function handleChangeSymbol(rowIndex, colIndex) {
+    // setActivePlayer(prevState => prevState==="X" ? "O" : "X")
+    setGameTurns(prevTurn => {
+      const currentPlayer = deriveActivePlayer(prevTurn)
+
+      const updateTurn = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurn]
+
+      return updateTurn
+    })
+  }
 
   return (
     <>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer==="X"}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer==="O"}/>
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <Gameboard onSelect={handleChangeSymbol} activeSymbol={activePlayer} turns={gameTurns}/>
+        {winner && <p>you won , {winner} !</p>}
+        <Gameboard onSelect={handleChangeSymbol} activeSymbol={activePlayer} board={gameboard} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </>
   )
 }
--- src/Component/Gameboard.jsx
+++ src/Component/Gameboard.jsx
@@ -1,24 +1,10 @@
-import { useState } from "react"
 
-const initialGameboard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
+export default function Gameboard({ onSelect, board }) {
 
-export default function Gameboard({ onSelect, turns }) {
-    let gameboard = initialGameboard
-
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameboard[row][col] = player
-    }
 
     return (
         <ol id="game-board">
-            {initialGameboard.map((row, rowIndex) =>
+            {board.map((row, rowIndex) =>
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) =>
