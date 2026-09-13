2b1ad043658ef179ff72012ee2fedd57c32b9fdd
sadegh shokhan <sadegh_shokhan@yahoo.com>
2026-09-13 15:39:08 +0330

    disable future added to gameboard button

--- src/Component/Gameboard.jsx
+++ src/Component/Gameboard.jsx
@@ -9,18 +9,24 @@ const initialGameboard = [
 export default function Gameboard({ onSelect, turns }) {
     let gameboard = initialGameboard
 
-        for (const turn of turns){
-            const {square , player} = turn ;
-            const {row , col} = square ;
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
 
-            gameboard[row][col] = player
-        }
+        gameboard[row][col] = player
+    }
 
     return (
         <ol id="game-board">
             {initialGameboard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
-                    {row.map((col, colIndex) => <li key={colIndex}><button onClick={()=>onSelect(rowIndex , colIndex)}>{col}</button></li>)}
+                    {row.map((playerSymbol, colIndex) =>
+                        <li key={colIndex}>
+                            <button onClick={() => onSelect(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                                {playerSymbol}
+                            </button>
+                        </li>
+                    )}
                 </ol>
             </li>)}
         </ol>
